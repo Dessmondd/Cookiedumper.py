41e9e27cdaffcfb6f75ee638511adad2344e8a8f
Dessmondd <97458634+Dessmondd@users.noreply.github.com>
2023-05-23 15:51:49 +0200

    Connecting to database, decrypt encrypted cookies.

--- Cookiedumper.py
+++ Cookiedumper.py
@@ -51,4 +51,25 @@ cursor = db.cursor()
 cursor.execute("""
     SELECT host_key, name, value, creation_utc, last_access_utc, expires_utc, encrypted_value 
     FROM cookies""")
+    key = get_encryption_key()
+    for host_key, name, value, creation_utc, last_access_utc, expires_utc, encrypted_value in cursor.fetchall():
+        if not value:
+            decrypted_value = decrypt_data(encrypted_value, key)
+        else:
+            decrypted_value = value
+        print(f"""
+        Host: {host_key}
+        Cookie name: {name}
+        Cookie value (decrypted): {decrypted_value}
+        Creation datetime (UTC): {get_chrome_datetime(creation_utc)}
+        Last access datetime (UTC): {get_chrome_datetime(last_access_utc)}
+        Expires datetime (UTC): {get_chrome_datetime(expires_utc)}
+        ===============================================================
+        """)
+        cursor.execute("""
+        UPDATE cookies SET value = ?, has_expires = 1, expires_utc = 99999999999999999, is_persistent = 1, is_secure = 0
+        WHERE host_key = ?
+        AND name = ?""", (decrypted_value, host_key, name))
+    db.commit()
+    db.close(
     
